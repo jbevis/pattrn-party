e7fe074735807c8e0a7e18e8c380df6c712e8377
Jack Bevis <jack.bevis@gmail.com>
2017-07-13 01:37:10 -0600

    Accordion tabs desktop and mobile, added basic styles and responsive design

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
+// desktop accordion tabs
 const openTab = (evt, cityName) => {
-    // Declare all variables
   let i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-    // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
+}
+
+//mobile accordion tabs
+const acc = document.getElementsByClassName("tablink-mobile");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  }
 }
